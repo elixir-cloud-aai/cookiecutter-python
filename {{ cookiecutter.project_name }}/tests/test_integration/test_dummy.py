995e0be7cf9384b4d5d567e02449a685572f358b
Javed Habib <jh4official@gmail.com>
2024-07-27 14:28:28 +0530

    fix

--- {{ cookiecutter.project_name }}/tests/test_integration/test_dummy.py
+++
@@ -1,9 +0,0 @@
-"""Dummy test file to avoid "Module has no tests" error."""
-
-
-def test_placeholder():
-    """Dummy test for CI workflow
-
-    This test is a placeholder for the integration tests. It is a workaround
-    to avoid the error "Module has no tests" when running the tests with pytest.
-    """ 
---
+++ {{ cookiecutter.project_name }}/tests/test_integration/test_init.py
@@ -0,0 +1,13 @@
+"""Unit tests for the __init__.py file."""
+
+from pathlib import Path
+
+import pytest
+
+@pytest.fixture
+def init_file_exists():
+    """Check if the __init__.py file exists."""
+    path_to_init_file = (
+        Path(__file__).parents[2] / '{{ cookiecutter.project_slug }}' / '__init__.py'
+        )
+    assert path_to_init_file.exists(), f"File {path_to_init_file} does not exist."
